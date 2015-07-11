194c44ca337fd8fda143dc82528c57b819704950
Ian Branum <ian@woodchucksystems.com>
2015-07-11 11:26:49 -0500

    Updating png generation

--- plot3.R
+++ plot3.R
@@ -8,12 +8,12 @@ plot3 <- function() {
      ydata2 <- dat1$Sub_metering_2
      ydata3 <- dat1$Sub_metering_3
      
+     png(file="plot3.png", width=480, height=480)
      plot(xdata, ydata1, type="l", xlab="", ylab="Energy sub metering")
      lines(xdata, ydata2, type="l", col="red")
      lines(xdata, ydata3, type="l", col="blue")
      
-     legend("topright", c("Sub metering 1", "Sub metering 2", "Sub metering 3"), col = c("black", "red", "blue"), lty=1)
-     dev.copy(png, file="plot3.png")
+     legend("topright",  c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c("black", "red", "blue"), lty=1)
      dev.off()
 }
 
